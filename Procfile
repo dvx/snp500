f37474b0478958a9981bc65ad8b13603ce3711d6
David Titarenco <david.titarenco@gmail.com>
2018-12-04 17:34:57 -0800

    redis implementation

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python snp500.py
--- const.py
+++ const.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv(), override=True)
 
-SP500_COMPANIES_PAGE = 'List_of_S%26P_500_companies'
+SNP500_COMPANIES_PAGE = 'List_of_S%26P_500_companies'
 WIKIPEDIA_API_PARSE = 'https://en.wikipedia.org/w/api.php?action=parse&page={}&format=json'
 WIKIPEDIA_API_REV = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles={}&rvprop=ids&format=json'
 PORT = os.getenv('PORT', default=80)
